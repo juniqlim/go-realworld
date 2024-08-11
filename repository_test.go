89bffcf8d439dc6059d232a3672b4346429b703d
juniq <juniqlim@gmail.com>
2024-08-11 22:55:26 +0900

    test: repository 테스트 오류 수정

--- repository_test.go
+++ repository_test.go
@@ -8,12 +8,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var testDB *sqlx.DB
+var (
+	repo UserRepository
+)
 
 // 테스트 환경을 위한 초기화
 func init() {
 	var err error
-	testDB, err = sqlx.Open("sqlite3", ":memory:")
+	testDB, err := sqlx.Open("sqlite3", ":memory:")
 	if err != nil {
 		panic(err)
 	}
@@ -30,13 +32,8 @@ func init() {
 		panic(err)
 	}
 
-	// 함수를 테스트하기 전에 실제 DB를 사용하지 않도록, 테스트용 DB를 사용하도록 설정합니다.
-	originalDB := db
-	db = testDB
-	defer func() { db = originalDB }() // 테스트 후 원래 DB로 복원
+	repo = userDBRepository(testDB)
 }
-
-var repo = userDBRepository(testDB)
 
 func TestCreateUserDB(t *testing.T) {
 	user := User{
